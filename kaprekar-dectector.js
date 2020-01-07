e634a7538a34fc91a3394db13ca144b974c16164
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-01-07 03:21:46 +0100

    connected UI to the file

--- kaprekar-dectector.js
+++ kaprekar-dectector.js
@@ -1,3 +1,46 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "0",
+    "timeOut": "8000",
+    "extendedTimeOut": "4000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+toastr.success('Welcome To Kaprekar Detector Site');
+document.getElementById('title').addEventListener('click', ()=>{
+    toastr.success('Welcome To Kaprekar Detector Site');
+})
+
+const container = document.querySelector('.error__container');
+const numberInput = document.getElementById('check__if__karprekar__input');
+numberInput.addEventListener('keyup', (e)=>{
+if(e.key === 'Enter'){
+ getNum()
+}
+})
+
+document.querySelector('.btn').addEventListener('click', ()=>{
+    getNum();
+})
+
+function getNum(){
+    const number = numberInput.value;
+    if(number === ''){
+        toastr.error('Input cannot be empty');
+    }else{
+        checkIsKaprekar(number);
+    }
+}
+
 function checkIsKaprekar(checkNum){
     //changing the number to a string and
     //checking the length of the number that was passed into the function
@@ -10,8 +53,17 @@ function checkIsKaprekar(checkNum){
 
     //then I added the first part of the squared number and the second part
     const sumOfNum = Number(firstNum) + Number(secondNum);
-
-    return  sumOfNum === checkNum ? `${checkNum} is a kaprekar number` : `${checkNum} is not a kaprekar number`;
+    if(sumOfNum == checkNum) {
+        const errordiv = document.createElement('div');
+        errordiv.classList.add('check__modal');
+        errordiv.innerHTML = `<i class="far fa-check-circle" id="check__icon"></i>
+        <span class="check__span">[YES]</span> <p>${checkNum} is a karprekar number because ${checkNum}<sup>2</sup> = ${squaredNum} -> ${firstNum} + ${secondNum} = ${sumOfNum} [which is equals to ${checkNum}]</p>`
+       return container.appendChild(errordiv);
+    } else if(sumOfNum !== checkNum){
+        const div = document.createElement('div');
+        div.classList.add('error__modal');
+        div.innerHTML = `<i class="far fa-times-circle" id="error__icon"></i>
+        <span class="error__span">[NO]</span> <p>${checkNum} is not a karprekar number</p>`
+       return container.appendChild(div);
+    }
 }
-
-console.log(checkIsKaprekar(45));
